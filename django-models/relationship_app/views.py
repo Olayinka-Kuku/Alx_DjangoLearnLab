22981fbad59a64e3b47ba8c12a0eee6e756257fb
Kuku Olayinka <olayinkakuku5@gmail.com>
2024-11-06 16:14:12 +0100

    modified:   views.py

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -13,20 +13,32 @@ from django.http import HttpResponseForbidden
 from .models import UserProfile
 from django.contrib.auth.decorators import user_passes_test
 
-# View for Admin users
-@user_passes_test(lambda user: user.userprofile.role == 'Admin')
+def is_admin(user):
+    return user.is_authenticated and user.profile.role == 'Admin'
+
+def is_librarian(user):
+    return user.is_authenticated and user.profile.role == 'Librarian'
+
+def is_member(user):
+    return user.is_authenticated and user.profile.role == 'Member'
+
+
+# Admin view
+@user_passes_test(is_admin)
 def admin_view(request):
-    return render(request, 'admin_view.html')
+    return render(request, 'relationship_app/admin_view.html')
 
-# View for Librarian users
-@user_passes_test(lambda user: user.userprofile.role == 'Librarian')
+
+# Librarian view
+@user_passes_test(is_librarian)
 def librarian_view(request):
-    return render(request, 'librarian_view.html')
+    return render(request, 'relationship_app/librarian_view.html')
 
-# View for Member users
-@user_passes_test(lambda user: user.userprofile.role == 'Member')
+
+# Member view
+@user_passes_test(is_member)
 def member_view(request):
-    return render(request, 'member_view.html')
+    return render(request, 'relationship_app/member_view.html')
 
 class SignUpView(CreateView):
     form_class = UserCreationForm
