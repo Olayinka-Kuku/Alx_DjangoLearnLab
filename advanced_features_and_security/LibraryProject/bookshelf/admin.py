7ccff4bc2cd573c25690eaf389429df14c6e192a
Kuku Olayinka <olayinkakuku5@gmail.com>
2024-11-12 15:28:58 +0100

    new file:   LibraryProject/__init__.py         new file:   LibraryProject/__pycache__/__init__.cpython-312.pyc         new file:   LibraryProject/__pycache__/settings.cpython-312.pyc         new file:   LibraryProject/__pycache__/urls.cpython-312.pyc         new file:   LibraryProject/asgi.py         new file:   LibraryProject/settings.py         new file:   LibraryProject/urls.py         new file:   LibraryProject/wsgi.py         new file:   README.md         new file:   bookshelf/CRUD_operations.md         new file:   bookshelf/__init__.py         new file:   bookshelf/__pycache__/__init__.cpython-312.pyc         new file:   bookshelf/__pycache__/admin.cpython-312.pyc         new file:   bookshelf/__pycache__/apps.cpython-312.pyc         new file:   bookshelf/__pycache__/models.cpython-312.pyc         new file:   bookshelf/admin.py         new file:   bookshelf/apps.py         new file:   bookshelf/create.md         new file:   bookshelf/delete.md         new file:   bookshelf/migrations/0001_initial.py         new file:   bookshelf/migrations/0002_customuser.py         new file:   bookshelf/migrations/__init__.py         new file:   bookshelf/migrations/__pycache__/0001_initial.cpython-312.pyc         new file:   bookshelf/migrations/__pycache__/0002_customuser.cpython-312.pyc         new file:   bookshelf/migrations/__pycache__/__init__.cpython-312.pyc         new file:   bookshelf/models.py         new file:   bookshelf/retrieve.md         new file:   bookshelf/tests.py         new file:   bookshelf/update.md         new file:   bookshelf/views.py         new file:   db.sqlite3         new file:   manage.py

---
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -0,0 +1,26 @@
+from django.contrib import admin
+from .models import Book
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {"fields": ("date_of_birth", "profile_photo")}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {"fields": ("date_of_birth", "profile_photo")}),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
+
+
+class BookAdmin(admin.ModelAdmin):
+    # Display these fields in the admin list view
+    list_display = ('title', 'author', 'publication_year')
+    # Add a filter sidebar to filter by publication_year
+    list_filter = ('publication_year',)
+    # Enable search functionality for title and author fields
+    search_fields = ('title', 'author')
+
+# Register the Book model with the custom admin settings
+admin.site.register(Book, BookAdmin)
