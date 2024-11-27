99c4e19bd8084fc864e5af0a7f1618956baff0ce
Kuku Olayinka <olayinkakuku5@gmail.com>
2024-11-27 17:17:39 +0100

    modified:   advanced-api-project/advanced_api_project/__pycache__/urls.cpython-312.pyc         modified:   advanced-api-project/advanced_api_project/urls.py         new file:   advanced-api-project/api/__pycache__/urls.cpython-312.pyc         new file:   advanced-api-project/api/__pycache__/views.cpython-312.pyc         modified:   advanced-api-project/api/urls.py

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -1,10 +1,12 @@
+# api/urls.py
+
 from django.urls import path
-from . import views
+from . import views  # Import the views from api/views.py
 
 urlpatterns = [
-    path('books/', views.BookListView.as_view(), name='book/list'),  # List all books
-    path('books/<int:pk>/', views.BookDetailView.as_view(), name='book-detail'),  # Get details of a single book
-    path('books/create', views.BookCreateView.as_view(), name='book-create'),  # Create a new book
-    path('books/update', views.BookUpdateView.as_view(), name='book-update'),  # Update an existing book
-    path('books/delete', views.BookDeleteView.as_view(), name='book-delete'),  # Delete a book
+    path('books/', views.BookListView.as_view(), name='book-list'),
+    path('books/<int:pk>/', views.BookDetailView.as_view(), name='book-detail'),
+    path('books/create/', views.BookCreateView.as_view(), name='book-create'),
+    path('books/update/<int:pk>/', views.BookUpdateView.as_view(), name='book-update'),
+    path('books/delete/<int:pk>/', views.BookDeleteView.as_view(), name='book-delete'),
 ]
