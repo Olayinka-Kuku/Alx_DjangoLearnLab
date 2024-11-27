cba734d0fec5ed46ca3d126b30b0451bc97ceca0
Kuku Olayinka <olayinkakuku5@gmail.com>
2024-11-27 18:01:49 +0100

    modified:   advanced-api-project/api/urls.py         modified:   advanced-api-project/api/views.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -3,8 +3,8 @@ from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticate
 from .models import Book
 from .serializers import BookSerializer
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.filters import SearchFilter, OrderingFilter
 from django_filters import rest_framework as filters
+from django_filters.rest_framework import SearchFilter, OrderingFilter
 
 class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
