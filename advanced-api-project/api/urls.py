0723ecbaf01c4f6b325b8b65e2217b4854f79a53
Kuku Olayinka <olayinkakuku5@gmail.com>
2024-11-27 17:06:13 +0100

    modified:   advanced-api-project/api/urls.py

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path('books/', views.BookListView.as_view(), name='book/list'),  # List all books
-    path('books/<int:pk>/', views.BookDetailView.as_view(), name='book/detail'),  # Get details of a single book
-    path('books/create/', views.BookCreateView.as_view(), name='book/create'),  # Create a new book
-    path('books/<int:pk>/update/', views.BookUpdateView.as_view(), name='book/update'),  # Update an existing book
-    path('books/<int:pk>/delete/', views.BookDeleteView.as_view(), name='book/delete'),  # Delete a book
+    path('books/<int:pk>/', views.BookDetailView.as_view(), name='book-detail'),  # Get details of a single book
+    path('books/create', views.BookCreateView.as_view(), name='book-create'),  # Create a new book
+    path('books/update', views.BookUpdateView.as_view(), name='book-update'),  # Update an existing book
+    path('books/delete', views.BookDeleteView.as_view(), name='book-delete'),  # Delete a book
 ]
