30580bf55967486c8092e1a53ee113766078e8ea
Kuku Olayinka <olayinkakuku5@gmail.com>
2024-12-05 16:06:01 +0100

    modified:   django_blog/__pycache__/settings.cpython-312.pyc         modified:   django_blog/settings.py

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -74,12 +74,18 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_blog',
+        'USER': 'root',
+        'PASSWORD': 'NewStrongPassword123!',
+        'HOST': 'localhost',
+        'PORT': '3306',  # Default MySQL port
     }
 }
+
 
 
 # Password validation
