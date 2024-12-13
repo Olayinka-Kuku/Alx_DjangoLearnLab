6d6ddfe614073d0df971f2a24d30c37c9fc5b126
Kuku Olayinka <olayinkakuku5@gmail.com>
2024-12-13 12:36:35 +0100

    minor changes to trigger commit

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -8,6 +8,7 @@ from accounts.models import CustomUser
 from rest_framework.views import APIView
 from django.contrib.contenttypes.models import ContentType
 from notifications.models import Notification
+from django.shortcuts import get_object_or_404
 
 class LikePost(APIView):
     permission_classes = [IsAuthenticated]
@@ -95,4 +96,36 @@ def user_feed(request):
     post_data = [{"id": post.id, "title": post.title, "content": post.content, "created_at": post.created_at} for post in posts]
     return Response(post_data, status=status.HTTP_200_OK)
     
+@api_view(['POST'])
+def like_post(request, pk):
+    # Get the post object
+    post = get_object_or_404(Post, pk=pk)
+    
+    # Check if the user has already liked the post
+    Like, created = Like.objects.get_or_create(user=request.user, post=post)
+    
+    # If a new like is created, generate a notification
+    if created:
+        # You can customize the notification message as needed
+        Notification.objects.create(
+            user=post.author,
+            message=f'{request.user.username} liked your post: {post.title}',
+            post=post
+        )
+        return Response({"message": "Post liked!"}, status=status.HTTP_201_CREATED)
+    else:
+        return Response({"message": "You have already liked this post."}, status=status.HTTP_400_BAD_REQUEST)
+    
 
+@api_view(['DELETE'])
+def unlike_post(request, pk):
+    # Get the post object
+    post = get_object_or_404(Post, pk=pk)
+    
+    # Get the like object if it exists
+    try:
+        like = Like.objects.get(user=request.user, post=post)
+        like.delete()  # Delete the like
+        return Response({"message": "Post unliked!"}, status=status.HTTP_204_NO_CONTENT)
+    except Like.DoesNotExist:
+        return Response({"message": "You have not liked this post yet."}, status=status.HTTP_400_BAD_REQUEST)
